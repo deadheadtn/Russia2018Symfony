f6734cb7ba98c6962ad02b5ca141a3282e10e806
nejmeddine khechine <nejmpotter@gmail.com>
2018-04-05 11:33:38 +0000

    first1

--- src/RUSSIA/PIBundle/RUSSIAPIBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace RUSSIA\PIBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class RUSSIAPIBundle extends Bundle
-{
-}
--- src/RUSSIA2/PIBundle/RUSSIA2PIBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace RUSSIA2\PIBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class RUSSIA2PIBundle extends Bundle
-{
-}
